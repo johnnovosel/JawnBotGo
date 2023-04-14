96ac4bd3da805f4ce345d77f8656e26e201cbfed
johnnovosel <johnnovosel96@gmail.com>
2023-04-13 23:32:59 -0500

    Command support, bulk user add

--- main.go
+++ main.go
@@ -22,14 +22,38 @@ func main() {
 
 	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
+	dg.AddHandler(handleCommand)
 
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	// grab all the users of the server and add them to the db
+	dg.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		// idk how to do it for multiple guilds rn lmao
+		if len(r.Guilds) > 0 {
+			guildID := r.Guilds[0].ID
+
+			// Fetch all members of the guild
+			members, err := dg.GuildMembers(guildID, "", 1000)
+			if err != nil {
+				fmt.Println("Error fetching guild members: ", err)
+				return
+			}
+
+			socialscore.BulkAddUser(members)
+		}
+	})
+
+	dg.AddHandler(func(s *discordgo.Session, r *discordgo.GuildMemberAdd) {
+		socialscore.AddUser(r.User.ID, r.User.Username)
+	})
+
+	dg.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuildMembers
 
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
+
+	registerCommands(dg)
 
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 
@@ -38,23 +62,72 @@ func main() {
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
+	// oh crap delete the commands so deficient people don't try to spam commands
+	deleteCommands(dg)
+
 	// Cleanly close down the Discord session.
 	dg.Close()
 
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all messages created by bots.
-	if m.Author.Bot {
-		return
-	}
-
 	// Create a new goroutine to handle the message.
 	go handleMessage(s, m.Message)
 }
 
 func handleMessage(s *discordgo.Session, m *discordgo.Message) {
-	if m.Content[0:8] == "!j addme" {
-		socialscore.AddUser(s, m)
+
+	// i hate pleb bot
+	if m.Author.ID == "168209080115134464" {
+		socialscore.CheckPointAdjustment(m)
+	}
+
+	//then we send the message to see if they gain or lose points
+	socialscore.CheckPointAdjustment(m)
+}
+
+func handleCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	if i.ApplicationCommandData().Name == "ss" {
+		command := i.ApplicationCommandData().Options[0].StringValue()
+
+		if command == "mystats" {
+			socialscore.GiveStatsForUser(s, i)
+		}
+	}
+}
+
+func registerCommands(s *discordgo.Session) {
+
+	cmd := &discordgo.ApplicationCommand{
+		Name:        "ss",
+		Description: "social score commands",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "command",
+				Description: "what Social Score command do you want",
+				Required:    true,
+			},
+		},
+	}
+	_, err := s.ApplicationCommandCreate(s.State.User.ID, "931418680074596452", cmd)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func deleteCommands(s *discordgo.Session) {
+	// get the list of commands
+	commands, err := s.ApplicationCommands(s.State.User.ID, "")
+	if err != nil {
+		fmt.Println("Error getting commands:", err)
+		return
+	}
+
+	for _, command := range commands {
+		err = s.ApplicationCommandDelete(s.State.User.ID, "", command.ID)
+		if err != nil {
+			fmt.Printf("Error deleting command %s: %s\n", command.Name, err)
+		}
 	}
 }
--- socialscore/socialscore.go
+++ socialscore/socialscore.go
@@ -2,8 +2,11 @@ package socialscore
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -11,11 +14,14 @@ import (
 type Data struct {
 	DiscordID string `json:"discordID"`
 	Score     int    `json:"score"`
+	Name      string `json:"name"`
 }
 
-var bigdata = make([]Data, 0)
+var bigdata = make(map[string]Data)
 
 func ReadUserFile() {
+	mySlice := make([]Data, 0)
+
 	// Read the file contents
 	fileData, err := os.ReadFile("socialscore\\db.json")
 	if err != nil {
@@ -23,16 +29,25 @@ func ReadUserFile() {
 	}
 
 	// Unmarshal the JSON data into a slice of Person structs
-	err = json.Unmarshal(fileData, &bigdata)
+	err = json.Unmarshal(fileData, &mySlice)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	for _, obj := range mySlice {
+		bigdata[obj.DiscordID] = obj
 	}
 }
 
 func WriteJSONFile() error {
 
+	var userSlice []Data
+	for _, user := range bigdata {
+		userSlice = append(userSlice, user)
+	}
+
 	// Marshal struct to JSON
-	jsonData, err := json.MarshalIndent(bigdata, "", "    ")
+	jsonData, err := json.MarshalIndent(userSlice, "", "    ")
 	if err != nil {
 		return err
 	}
@@ -46,31 +61,99 @@ func WriteJSONFile() error {
 	return nil
 }
 
-func AddUser(s *discordgo.Session, m *discordgo.Message) {
-	//check if the user already exists
-	doesExists := alreadyExists(m.Author.ID)
-
-	if doesExists {
-		s.ChannelMessageSend(m.ChannelID, "You is already added")
-		return
-	}
-
+func AddUser(userID string, name string) {
 	user := &Data{
-		DiscordID: m.Author.ID,
+		DiscordID: userID,
 		Score:     100,
+		Name:      name,
 	}
 
-	bigdata = append(bigdata, *user)
+	bigdata[user.DiscordID] = *user
+}
+
+func BulkAddUser(members []*discordgo.Member) {
+
+	for _, member := range members {
+
+		//check if the user already exists
+		doesExists := AlreadyExists(member.User.ID)
+
+		if doesExists {
+			break
+		} else {
+			AddUser(member.User.ID, member.User.Username)
+		}
+	}
 
 	WriteJSONFile()
 }
 
-func alreadyExists(userID string) bool {
-	for i := 0; i < len(bigdata); i++ {
-		if bigdata[i].DiscordID == userID {
-			return true
-		}
+func AlreadyExists(userID string) bool {
+	_, exists := bigdata[userID]
+
+	return exists
+}
+
+func updatePoints(userID string, amount int) {
+	user := &Data{
+		DiscordID: userID,
+		Score:     bigdata[userID].Score + amount,
+		Name:      bigdata[userID].Name,
 	}
 
-	return false
+	bigdata[userID] = *user
+
+	WriteJSONFile()
+}
+
+func GiveStatsForUser(s *discordgo.Session, i *discordgo.InteractionCreate) {
+
+	userID := ""
+	if i.Member != nil {
+		userID = i.Member.User.ID
+	}
+
+	if i.User != nil {
+		userID = i.User.ID
+	}
+
+	responseString := "<@" + bigdata[userID].DiscordID + ">'s score is " + strconv.Itoa(bigdata[userID].Score)
+
+	embed := &discordgo.MessageEmbed{
+		Title:       "Social Score",
+		Description: responseString,
+		Color:       808000,
+	}
+
+	responseBuilder(s, i, embed)
+}
+
+// man i love building beans
+func responseBuilder(s *discordgo.Session, i *discordgo.InteractionCreate, embed *discordgo.MessageEmbed) {
+	response := &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{embed},
+		},
+	}
+
+	err := s.InteractionRespond(i.Interaction, response)
+	if err != nil {
+		fmt.Println("Error sending response:", err)
+	}
+}
+
+func CheckPointAdjustment(m *discordgo.Message) {
+	message := m.Content
+
+	switch true {
+	case strings.Contains(message, "jackbox"):
+		{
+			updatePoints(m.Author.ID, -1)
+		}
+	case m.Author.ID == "168209080115134464":
+		{
+			updatePoints(m.Author.ID, -1)
+		}
+	}
 }
